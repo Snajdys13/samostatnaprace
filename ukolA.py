69e1a7b7b6ab5d2bad8a4421d78b70790562a6b8
Snajdys13 <erik.snajdr@infis.cz>
2025-04-01 08:28:54 +0200

    Add files via upload

---
+++ ukolA.py
@@ -0,0 +1,12 @@
+print("Zadejte 3 jídla, která jste během dne snědl/a")
+jidlo1 = input()
+jidlo2 = input()
+jidlo3 = input()
+
+print("Zadejte počet kalorií každého jídla aspoň odhadem")
+kalorie1 = int(input())
+kalorie2 = int(input())
+kalorie3 = int(input())
+celkoveKalorie = (kalorie1 + kalorie2 + kalorie3)
+
+print (f"Celkový počet kalorií je {celkoveKalorie}")
