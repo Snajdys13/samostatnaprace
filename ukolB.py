59208f5a7bcc686ce82931d8aac56a9d8f70fbb3
Snajdys13 <erik.snajdr@infis.cz>
2025-04-01 08:38:00 +0200

    Add files via upload

---
+++ ukolB.py
@@ -0,0 +1,7 @@
+print("Zadejte 2 aktivity které jste dnes dělal/a")
+input()
+
+print("Nyní zadejte jak dlouho jste dané aktivity dělal/a v minutách")
+aktivita1 = int(input())
+aktivita2 = int(input())
+minuta = 5 
