debf929db2ef06107b910929640228ac30a980a1
Snajdys13 <erik.snajdr@infis.cz>
2025-04-22 11:12:08 +0200

    Add files via upload

---
+++ poleopacko.py
@@ -0,0 +1,9 @@
+znamecky = [1, 2, 3, 4, 5]
+
+soucet = znamecky[0] + znamecky[1] + znamecky[2] + znamecky[3] + znamecky[4]
+
+
+for znamecka in znamecky:
+    prumer = soucet/len (znamecky)
+print(soucet)
+print(prumer)
