efc21b71d82be3a55f57457af35ac19adda12f01
Snajdys13 <erik.snajdr@infis.cz>
2025-04-29 08:39:31 +0200

    Create skore.zapasu.py

---
+++ skore.zapasu.py
@@ -0,0 +1,20 @@
+pole = [0, 10, 20, 30, 35, 40, 45, 50, 55, 60]
+
+print(sum(pole))
+
+print(sum(pole))
+
+print(max(pole))
+print(min(pole))
+
+hraci = len(pole)
+polovinaHracu = hraci/2
+
+for pole in range >50:
+     print(pole)
+
+if polovinaHracu + pole >50:
+     print("Výborný výkon")
+
+if polovinaHracu + pole<50:
+     print("Můžete to příště zkusit lépe")
