98fcd0990d14856383e95fbff2fcc813cea240a1
Snajdys13 <erik.snajdr@infis.cz>
2025-05-13 08:40:42 +0200

    Add files via upload

---
+++ pole.procvicovani.py
@@ -0,0 +1,59 @@
+#jidlo
+jidla = ["spagety", "pizza", "gulas"]
+print(jidla [0])
+print(jidla[1])
+print(jidla[2])
+
+#predmet
+ajina = [1, 1, 1, 2, 1, 1, 1, 2, 1, 1]
+soucet = sum(ajina)
+print(soucet)
+pocetZnamek = len(ajina)
+print(pocetZnamek)
+print("-----------------")
+ajina.append(1)
+for znamky in ajina:
+    print(znamky)
+
+#zvirata
+zvirata = ["lev", "kocka", "pes", "gepard"]
+print(zvirata[1])
+print(zvirata[-1])
+
+#libovolna cisla
+seznam = [1, 3, 5]
+print(seznam)
+seznam.append(13)
+seznam.append(2)
+seznam.append(7)
+print(seznam)
+
+#oblibene filmy
+filmy = ["Gladiator 2", "Gran Turismo", "Avengers Endgame"]
+print(filmy[0])
+print(filmy[1])
+print(filmy[2])
+
+#seznam tri cisel
+cisla = [1, 3, 5]
+print(cisla)
+cisla[1]= 999
+print(cisla)
+
+#pet libovolnych cisel
+cislicka = [0, 1, 2, 3, 4]
+soucet = sum(cislicka)
+print(soucet)
+
+#policko
+policko = [1, 2, 3, 4, 5]
+delkaPole = len(policko)
+print(delkaPole)
+
+#prumer znamek
+poleno = [1, 2, 3, 4, 5]
+prumer = sum(poleno)/len(poleno)
+print(prumer)
+
+#seznam ovoce
+seznamOvoce = ["kiwi", "banan", "ananas"]
